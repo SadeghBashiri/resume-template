2013ce3136b83402eb06eea5965533397b46954b
manred1987 <manred1987@gmail.com>
2025-04-17 11:27:19 +0330

    remove src dir

--- src/app/fwk/i18n/i18n.module.ts
+++
@@ -1,9 +0,0 @@
-import { MissingTranslationHandler, MissingTranslationHandlerParams } from '@ngx-translate/core';
-import { Injectable } from "@angular/core";
-
-@Injectable()
-export class AppMissingTranslationHandler implements MissingTranslationHandler {
-    handle(params: MissingTranslationHandlerParams) {
-        return params.key;
-    }
-}
