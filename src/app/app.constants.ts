76450b052e6a077ce3a2747337deb8c0f036bff8
manred1987 <manred1987@gmail.com>
2025-04-18 12:53:18 +0330

    modify navbar

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -9,7 +9,7 @@ export class AppConstants {
 
   public static DATE = '2025-04-17';
   public static APP_TITLE = 'CV';
-  public static AUTHOR_NAME = 'Sadegh Bashiri---';
+  public static AUTHOR_NAME = 'Sadegh Bashiri';
   public static APP_TITLE_VERSION: string = AppConstants.APP_TITLE + ' - ' + AppConstants.DATE;
 
   public static ANGULAR_VERSION = 'Ng ' + VERSION.full;
